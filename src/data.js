9f281f40c7fbe038cb34cf253f64898152854bcf
bet_h <bettybhan@gmail.com>
2022-11-29 16:13:17 -0500

    Add and update travel data

--- src/data.js
+++ src/data.js
@@ -1,11 +1,32 @@
 export default [
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-    }
+        id: 1,
+        title: "Eiffel Tower",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/3VYqxfsPkxQDYPEJ8",
+        startDate: "13 Mar, 2019",
+        endDate: "20 Mar, 2019",
+        description: "Gustave Eiffel's iconic, wrought-iron 1889 tower, with steps and elevators to observation decks.",
+        imageUrl: "https://unsplash.com/photos/Q0-fOL2nqZc"
+    },
+    {   
+        id: 2,
+        title: "Seoul",
+        location: "South Korea",
+        googleMapsUrl: "https://goo.gl/maps/UQXZhPx6CTjCGrFM6",
+        startDate: "27 Sept, 2018",
+        endDate: "13 Sept, 2018",
+        description: "Seoul, the capital of South Korea, is a huge metropolis where modern skyscrapers, high-tech subways and pop culture meet Buddhist temples, palaces and street markets.",
+        imageUrl: "https://unsplash.com/photos/qegMLAiTBA4"
+    },
+    {   
+        id: 3,
+        title: "Fuerteventura",
+        location: "Spain",
+        googleMapsUrl: "https://goo.gl/maps/HejctZDNXUstZCPJ9",
+        startDate: "26 Sept, 2006",
+        endDate: "1 Oct, 2006",
+        description: "White-sand beaches & year-round moderate climate in this island popular for water sports.",
+        imageUrl: "https://unsplash.com/photos/j7heFwsIq7U"
+    }    
 ]
